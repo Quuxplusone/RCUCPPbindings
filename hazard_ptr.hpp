848d1b3980b6e17b8040ddbcc8f017d9f6271c27
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2016-09-23 21:10:13 -0700

    Major, probably at least partly problematic, changes.
    This rips out a lot of "knobs" (tc_policy, rem_policy) whose
    usage I was unclear on — I suspect they could become merely a
    QoI issue controlled by vendor-specific global macros.
    
    This also rips out the explicit dependency on `std::atomic`,
    because the old `protect` function can actually be implemented
    in "client space" as far as I know.
    
    This rips out the explicit dependency on `std::pmr::memory_resource`,
    because the `hazard_domain` can actually accept any allocator type
    and type-erase that allocator itself, deep inside, where the client
    doesn't have to care about it.
    
    This makes `hazard_ptr` moveable, because I'm unaware of any reason
    it shouldn't be moveable. (Notice that `hazard_ptr` holds a handle
    to a hazard pointer, but it doesn't literally represent a hazard
    pointer itself. Hazard pointers themselves are stored in arrays or
    something, allocated by the `hazard_domain` and never seen directly
    by any client code. Hazard pointers themselves don't move. But we
    can pass around the handles just fine.)
    
    This leaves `enable_hazard_ptr` as a CRTP template, even though
    I've lost track of the reason it needs to be one. In Paul's RCU
    implementation, `enable_rcu` needs to be a CRTP template because
    it provides its derived class with a `.retire()` method. But in
    Maged's hazptr implementation, you don't say `foo.retire({deleter})`;
    you say `domain.retire(foo, {deleter})`.  Quite possibly we *should*
    flip this around to be `foo.retire(domain, {deleter})`, but I'm
    not sure about that yet.

--- hazard_ptr.hpp
+++ hazard_ptr.hpp
@@ -1,7 +1,5 @@
-#include <atomic>
-#include <functional>
 #include <memory>
-#include <experimental/memory_resource>
+#include <type_traits>
 
 namespace std {
 
@@ -14,14 +12,16 @@ extern hazard_domain default_hazard_domain;
  *
  * Base class template for objects protected by hazard pointers.
  */
-template <typename T, typename Allocator = std::allocator<T>>
+// I actually can't see any reason to depend on type T here.
+// This could be a plain old base class, not a CRTP template?
+template <typename T>
 class enable_hazard_ptr {
   /* Pointer used in constructing lists of removed objects awaiting
      reclamation, without requiring additional allocation. */
-  enable_hazard_ptr *next_removed_;
-
-  /* Pointer to allocator to be used to reclaim object. */
-  Allocator *alloc_;
+  // This is analogous to struct rcu_head in <urcu.h>.
+  // We might also need a single function pointer here, raising the overhead
+  // to 16 bytes per enable_hazard_ptr object; that's what <urcu.h> does.
+  void *next_removed_;
 };
 
 /** hazard_ptr
@@ -29,27 +29,26 @@ class enable_hazard_ptr {
  * Guard class template for RAII automatic allocation and release of hazard
  * pointers, and interface for user calls to hazard pointer functions.
  */
-template<typename T, typename Allocator = std::allocator<T>>
+template<typename T>
 class hazard_ptr {
+  static_assert(is_base_of<enable_hazard_ptr<T>, T>::value);
+
 public:
-  enum tc_policy { cache, nocache };
+  // hazard_ptr is a move-only type.
+  hazard_ptr(hazard_ptr&&) noexcept;
+  hazard_ptr& operator=(hazard_ptr&&) noexcept;
 
-  hazard_ptr(const hazard_ptr&) = delete;
-  hazard_ptr(hazard_ptr&&) = delete;
-  hazard_ptr& operator=(const hazard_ptr&) = delete;
-  hazard_ptr& operator=(hazard_ptr&&) = delete;
-
-  hazard_ptr(tc_policy tc = tc_policy::cache, hazard_domain *control_block = &default_hazard_domain);
+  hazard_ptr(hazptr_domain& domain = default_hazard_domain);
   ~hazard_ptr();
 
-  bool protect(const T *ptr, const std::atomic<T *>& src) noexcept;
-  void set(const T *ptr) noexcept;
-  void clear() noexcept;
+  void protect(const T *ptr) noexcept;
+  void reset() noexcept;
 
-  void swap(hazard_ptr& other) noexcept;
+  void swap(hazard_ptr&) noexcept;
 };
 
-void swap(hazard_ptr& a, hazard_ptr& b) noexcept
+template<typename T>
+void swap(hazard_ptr<T>& a, hazard_ptr<T>& b) noexcept
 {
   return a.swap(b);
 }
@@ -60,20 +59,21 @@ void swap(hazard_ptr& a, hazard_ptr& b) noexcept
  */
 class hazard_domain {
 public:
-  enum rem_policy { priv, shared };
-
+  // hazard_domain is a non-moveable type.
   hazard_domain(const hazard_domain&) = delete;
-  hazard_domain(hazard_domain&&) = delete;
-  hazard_domain& operator=(const hazard_domain&) = delete;
-  hazard_domain& operator=(hazard_domain&&) = delete;
 
-  constexpr hazard_domain(std::pmr::memory_resource *);
+  // The user can pass in an allocator used for the hazard pointer arrays themselves.
+  // We can store this allocator type-erasedly, since allocation is rare enough that
+  // we don't care about its cost.
+  template<typename Allocator = std::allocator<void*>>
+  constexpr hazard_domain(Allocator alloc = {});
 
   ~hazard_domain();
 
-  template<typename T, typename Allocator>
-  void reclaim(enable_hazard_ptr<T, Allocator> *ptr,
-               rem_policy rem = rem_policy::priv);
+  // Equivalent to d(ptr), except deferred until there are no hazard pointers
+  // remaining in this domain that still refer to ptr.
+  template<typename T, typename Deleter = default_delete<T>>
+  void retire(T *ptr, Deleter d = {});
 };
 
 } // namespace std
